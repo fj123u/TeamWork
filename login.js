a1f75318db9186a31da32074d95c1cf2feeaf521
fj123u <theo.girard.1807@gmail.com>
2026-04-03 12:14:44 +0200

    ajout du systeme de login

---
+++ login.js
@@ -0,0 +1,37 @@
+
+// Recuperation des elements principaux
+const form = document.getElementById("form-login");
+const erreur = document.getElementById("erreur");
+
+// Recuperation des champs du formulaire
+const inputPseudo = document.getElementById("pseudo");
+const inputPassword = document.getElementById("password");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const pseudo = inputPseudo.value.trim();
+    const password = inputPassword.value.trim();
+    // Validation : on s’arrete la premiere erreur
+    if (pseudo === "") 
+    {
+        erreur.textContent = "Le pseudo est obligatoire.";
+        return;
+    }
+    if (pseudo.length < 3) 
+    {
+        erreur.textContent = "Le pseudo est trop court.";
+        return;
+    }
+    if (password === "") 
+    {
+        erreur.textContent = "Le mot de passe est obligatoire.";
+        return;
+    }
+    if (password.length < 6) 
+    {
+        erreur.textContent = "Le mot de passe est trop court.";
+        return;
+    }
+    // Si tout est OK : effacer l’erreur
+    erreur.textContent = "";
+});
