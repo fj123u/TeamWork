7c4b73634bd2906cdb1865f125b96090154892c0
fj123u <theo.girard.1807@gmail.com>
2026-04-20 08:49:52 +0200

    réparation du login

--- login/login.js
+++ login/login.js
@@ -1,38 +1,30 @@
 
-// Recuperation des elements principaux
 const form = document.getElementById("form-login");
 const erreur = document.getElementById("erreur");
 
-// Recuperation des champs du formulaire
 const inputPseudo = document.getElementById("pseudo");
 const inputPassword = document.getElementById("password");
 
-form.addEventListener("submit", function(event)
-{
+form.addEventListener("submit", function (event) {
     event.preventDefault();
     const pseudo = inputPseudo.value.trim();
     const password = inputPassword.value.trim();
-    // Validation : on s’arrete la premiere erreur
-    if (pseudo === "") 
-    {
+    if (pseudo === "") {
         erreur.textContent = "Le pseudo est obligatoire.";
         return;
     }
-    if (pseudo.length < 3) 
-    {
+    if (pseudo.length < 3) {
         erreur.textContent = "Le pseudo est trop court.";
         return;
     }
-    if (password === "") 
-    {
+    if (password === "") {
         erreur.textContent = "Le mot de passe est obligatoire.";
         return;
     }
-    if (password.length < 6) 
-    {
+    if (password.length < 6) {
         erreur.textContent = "Le mot de passe est trop court.";
         return;
     }
-    // Si tout est OK : effacer l’erreur
     erreur.textContent = "";
+    form.reset();
 });
