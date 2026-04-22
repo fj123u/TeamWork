859d1e4612e244c28e9b82c26be87fd8ef6cfe1c
fj123u <theo.girard.1807@gmail.com>
2026-04-22 09:06:45 +0200

    ajout du système de signin en checkant si l'email et le password correspondent bien

--- signin/signin.js
+++ signin/signin.js
@@ -1,20 +1,20 @@
+let users = [];
+let a = false;
 
 const form = document.getElementById("form-signin");
 const erreur = document.getElementById("erreur");
 
-const inputPseudo = document.getElementById("pseudo");
+const inputEmail = document.getElementById("email");
 const inputPassword = document.getElementById("password");
+
+chargerUsers();
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    const pseudo = inputPseudo.value.trim();
+    const email = inputEmail.value.trim();
     const password = inputPassword.value.trim();
-    if (pseudo === "") {
-        erreur.textContent = "Le pseudo est obligatoire.";
-        return;
-    }
-    if (pseudo.length < 3) {
-        erreur.textContent = "Le pseudo est trop court.";
+    if (!validateEmail(email)) {
+        erreur.textContent = "L'email n'est pas valide.";
         return;
     }
     if (password === "") {
@@ -25,7 +25,32 @@ form.addEventListener("submit", function (event) {
         erreur.textContent = "Le mot de passe est trop court.";
         return;
     }
+    for (let i = 0; i < users.length; ++i) {
+        if (users[i].email == email) {
+           if (users[i].password == password)
+           {
+                a = true;
+           }
+        }
+    }
+    if (!a)
+    {
+        erreur.textContent = "Au moins une des valeurs saisies est incorrect.";
+        return;
+    }
     erreur.textContent = "";
     window.location.href = "../homepage/homepage.html";
     form.reset();
 });
+
+function validateEmail(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+}
+
+function chargerUsers() {
+    const data = localStorage.getItem("user");
+    if (data !== null) {
+        users = JSON.parse(data);
+    }
+}
